226035157c1731e0f7d25dc161e4eddcdb29f8aa
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-04-10 15:14:48 +0200

    Add go back button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,4 +28,8 @@ export class HeroDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
+
+  goBack() {
+    this.location.back();
+  }
 }
